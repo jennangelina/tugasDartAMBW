2e7935e80158896e5290ffc88bc1297ed78eb948
jennangelina <jennangelina09@gmail.com>
2022-03-05 12:34:20 +0700

    Commit 2 Nomor 2

--- bin/tugas_dart1.dart
+++ bin/tugas_dart1.dart
@@ -1,3 +1,50 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+   // NO 1
+
+   // NO 2
+  stdout.write("Masukkan angka genap: ");
+  String? input2 = stdin.readLineSync();
+
+  int n2 = int.parse(input2!);
+
+  int angka1 = 1;
+  int angka2 = n2;
+  if (n2 > 9) {
+    angka2 = (n2 % 10) + 1;
+  }
+
+  for (int i = 0; i < n2; i++) {
+    //kiri
+    for (int a = 0; a < n2 - i; a++) {
+      stdout.write(angka1);
+
+      if (angka1 == n2 || angka1 == 9) {
+        angka1 = 1;
+      } else {
+        angka1++;
+      }
+    }
+
+    //spasi
+    for (int s = 0; s < i; s++) {
+      stdout.write("  ");
+    }
+
+    //kanan
+    for (int b = 0; b < n2 - i; b++) {
+      stdout.write(angka2);
+      if (angka2 == 1) {
+        angka2 = n2;
+        if (n2 > 9) {
+          angka2 = 9;
+        }
+      } else {
+        angka2--;
+      }
+    }
+
+    print("");
+  }
 }
